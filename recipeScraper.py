3ea4bbb89c83f0c3cccaef448ce844a5999dc4f7
Ethan <hartzell.ethan@gmail.com>
2018-04-08 18:13:06 -0500

    Stock_Test
    tested scraping tutorial

--- recipeScraper.py
+++ recipeScraper.py
@@ -1,4 +1,11 @@
-x = 3
-y = 7
-print(x+y)
-print(y+x)
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+quote_page = "http://www.bloomberg.com/quote/SPX:IND"
+page = urlopen(quote_page)
+soup = BeautifulSoup(page, "html.parser")
+name_box = soup.find("h1", attrs={"class": "name"})
+name = name_box.text.strip() # strip() is used to remove starting and trailing
+print(name)
+price_box = soup.find("div", attrs={"class":"price"})
+price = price_box.text
+print(price)
