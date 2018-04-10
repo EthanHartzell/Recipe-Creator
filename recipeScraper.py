02fefbdc8d1ae595859cb90fb824059ed9b988b0
Ethan <hartzell.ethan@gmail.com>
2018-04-09 23:33:56 -0500

    Added all recipes
    Currently have a chocolate chip cookie recipe.  Am able to pull html for ingrident and quantity.  Not yet able to put it in a data frame.  This is the next step.

--- recipeScraper.py
+++ recipeScraper.py
@@ -1,13 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-quote_page = "http://www.bloomberg.com/quote/SPX:IND"
+quote_page = "https://www.allrecipes.com/recipe/15300/tinas-shortbread-chocolate-chip-cookies/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%202"
 page = urlopen(quote_page)
-soup = BeautifulSoup(page, "html.parser")
-name_box = soup.find("h1", attrs={"class": "name"})
-name = name_box.text.strip() # strip() is used to remove starting and trailing
+soup = BeautifulSoup(html_doc, "html.parser")
+name_box = soup.find_all("span", attrs={"itemprop": "ingredients"})
+name = name_box.text.strip() 
 print(name)
-price_box = soup.find("div", attrs={"class":"price"})
-price = price_box.text
-print(price)
-#test
-#ethan test
+name_box = soup.find_all("span", attrs={"itemprop": "ingredients"})
+   
+
+print(name_box)
+
