285aec3f0742e78aa5b8af808cb6dec34bb390f1
Ethan <hartzell.ethan@gmail.com>
2018-04-14 16:39:10 -0500

    Can now isolate just ingredient strings
    Next step is to parse strings to isolate quantity and type.  Convert all quantities to grams.
    
    - idea, use ingredient quantity as weight vector for model

--- recipeScraper.py
+++ recipeScraper.py
@@ -2,12 +2,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 quote_page = "https://www.allrecipes.com/recipe/15300/tinas-shortbread-chocolate-chip-cookies/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%202"
 page = urlopen(quote_page)
-soup = BeautifulSoup(html_doc, "html.parser")
-name_box = soup.find_all("span", attrs={"itemprop": "ingredients"})
-name = name_box.text.strip() 
-print(name)
-name_box = soup.find_all("span", attrs={"itemprop": "ingredients"})
-   
+soup = BeautifulSoup(page, 'html.parser')
+name_box = soup.find_all(itemprop = 'ingredients');
 
-print(name_box)
+for i in range(0,len(name_box)):
+    print(name_box[i].contents)
+
 
