ad06f30bb548edc2102965dae333c92404f565f0
Ethan <hartzell.ethan@gmail.com>
2018-04-15 14:03:31 -0500

    All REcipies
    Can now scrape ingredients from all recipies on search page

--- recipeScraper.py
+++ recipeScraper.py
@@ -1,11 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-quote_page = "https://www.allrecipes.com/recipe/15300/tinas-shortbread-chocolate-chip-cookies/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%202"
-page = urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find_all(itemprop = 'ingredients');
 
-for i in range(0,len(name_box)):
-    print(name_box[i].contents)
+cookie_page = "https://www.allrecipes.com/search/results/?wt=chocolate%20chip%20cookies&sort=re"
+cookies = urlopen(cookie_page)
+cookieSoup = BeautifulSoup(cookies,'html.parser')
 
-
+for link in cookieSoup.find_all(class_ ="fixed-recipe-card__h3"):
+    page = urlopen((link.a.get('href')))
+    soup = BeautifulSoup(page, 'html.parser')
+    name_box = soup.find_all(itemprop = 'ingredients')
+    for i in range(0,len(name_box)):
+        print(name_box[i].contents)
+        
