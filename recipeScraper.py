dc9b42affb38aa5661904263f23914d49c430fdf
Ethan <hartzell.ethan@gmail.com>
2018-04-15 17:11:51 -0500

    Function That pulls it all for arbitrary ingredient
    WhooHoo

--- recipeScraper.py
+++ recipeScraper.py
@@ -1,14 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-cookie_page = "https://www.allrecipes.com/search/results/?wt=chocolate%20chip%20cookies&sort=re"
-cookies = urlopen(cookie_page)
-cookieSoup = BeautifulSoup(cookies,'html.parser')
 
-for link in cookieSoup.find_all(class_ ="fixed-recipe-card__h3"):
-    page = urlopen((link.a.get('href')))
-    soup = BeautifulSoup(page, 'html.parser')
-    name_box = soup.find_all(itemprop = 'ingredients')
-    for i in range(0,len(name_box)):
-        print(name_box[i].contents)
-        
+def scrapeIngredients(Food):
+    cookie_page = "https://www.allrecipes.com/search/results/?wt=" + Food + "&sort=re"
+    cookies = urlopen(cookie_page)
+    cookieSoup = BeautifulSoup(cookies,'html.parser')
+    for link in cookieSoup.find_all(class_ ="fixed-recipe-card__h3"):
+        print("#####RECIPIE##########################")
+        page = urlopen((link.a.get('href')))
+        soup = BeautifulSoup(page, 'html.parser')
+        name_box = soup.find_all(itemprop = 'ingredients')
+        for i in range(0,len(name_box)):
+            print(name_box[i].contents)
+
+
+
+scrapeIngredients("chocolatechipcookies")
+
+scrapeIngredients("chocolatechipcookies")
